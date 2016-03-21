74383a07e136dda528b3ae8608a60720cca6fe80
OlgaGavr <olgagavryliuk17@gmail.com>
2016-03-21 13:17:16 +0200

    domain class

---
+++ src/main/java/com/javaee/naukmaplaces/domain/Place.java
@@ -0,0 +1,92 @@
+package com.javaee.naukmaplaces.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "PLACES")
+public class Place {
+
+	@Id
+	@Column(name = "PID")
+	@GeneratedValue
+	private Integer pid;
+
+	@Column(name = "TYPE")
+	private String type;
+
+	@Column(name = "NAME")
+	private String name;
+
+	@Column(name = "BUILDING")
+	private String building;
+
+	@Column(name = "STAGE")
+	private Integer stage;
+
+	@Column(name = "AUD")
+	private Integer aud;
+
+	@Column(name = "INFO")
+	private String info;
+
+	public Integer getPid() {
+		return pid;
+	}
+
+	public void setPid(Integer pid) {
+		this.pid = pid;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getBuilding() {
+		return building;
+	}
+
+	public void setBuilding(String building) {
+		this.building = building;
+	}
+
+	public Integer getStage() {
+		return stage;
+	}
+
+	public void setStage(Integer stage) {
+		this.stage = stage;
+	}
+
+	public Integer getAud() {
+		return aud;
+	}
+
+	public void setAud(Integer aud) {
+		this.aud = aud;
+	}
+
+	public String getInfo() {
+		return info;
+	}
+
+	public void setInfo(String info) {
+		this.info = info;
+	}
+
+}
